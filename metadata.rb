e210874891c69ba44cb264f54ef5ec121fc527df
trotttrotttrott <trott@odaacabeef.com>
2013-03-03 13:46:11 -0800

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'application_rest-tail'
+maintainer       'trotttrotttrott'
+maintainer_email ''
+license          'Apache 2.0'
+description      'Deploys and configures rest-tail'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+depends 'application_ruby'
---
+++ recipes/default.rb
@@ -0,0 +1,7 @@
+application 'rest-tail' do
+  path '/usr/local/www/rest-tail'
+  repository 'https://github.com/trotttrotttrott/rest-tail.git'
+
+  unicorn do
+  end
+end
