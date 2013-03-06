b75e6b1e0a3f566eebc96321f90173323c28900a
trotttrotttrott <trott@odaacabeef.com>
2013-03-05 19:37:09 -0800

    generate settings.yml file :before_deploy

--- providers/unicorn.rb
+++ providers/unicorn.rb
@@ -2,13 +2,21 @@ action :before_compile do
 end
 
 action :before_deploy do
-  %w(log tmp/pids tmp/sockets).each do |dir|
+
+  %w(log bundle tmp/pids tmp/sockets).each do |dir|
     directory "#{new_resource.path}/shared/#{dir}" do
       owner new_resource.owner
       group new_resource.group
       mode '0755'
       recursive true
     end
+  end
+
+  template "#{new_resource.path}/shared/settings.yml" do
+    source "settings.yml.erb"
+    owner new_resource.owner
+    group new_resource.group
+    mode '644'
   end
 end
 
@@ -22,7 +30,7 @@ action :before_restart do
   new_resource = @new_resource
   unicorn_config "/etc/unicorn/#{new_resource.name}.rb"
   Chef::Log.info "Running bundle install"
-  execute 'bundle install --path=vendor/bundle --without development,test' do
+  execute "bundle install --path=#{new_resource.path}/shared/bundle --without development,test" do
     cwd new_resource.release_path
     user new_resource.owner
     environment new_resource.environment
